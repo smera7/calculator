6d34eabaa97787615d8d856f378f549fbe99910c
Smera <smera@berkeley.edu>
2023-12-20 21:12:54 -0800

    made it work for 2

--- script.js
+++ script.js
@@ -24,37 +24,50 @@ function divide(a, b) {
 
 function calculate() {
     // Get the selected option value
-    var selectedOption = document.getElementById("operator").value;
+    var selectedOption = document.getElementById("operator").selectedOptions[0].textContent;
     var a = parseInt(document.getElementById("num1").value);
     var b = parseInt(document.getElementById("num2").value);
+    console.log(a, b, selectedOption);
     let result;
     // Perform a function based on the selected option
     switch (selectedOption) {
-      case "add":
+      case "Add":
         // Code for Option 1
         result = add(a, b);
-      case "subtract":
+        console.log(result);
+        break;
+      case "Subtract":
         // Code for Option 2
         result = subtract(a, b);
-      case "multiply":
+        break;
+      case "Multiply":
         // Code for Option 3
         result = multiply(a, b);
-      case "divide":
+        break;
+      case "Divide":
         // Code for Option 4
         result = divide(a, b);
+        break;
+
     }
+
+    var outputElement = document.getElementById("output");
+    outputElement.textContent = result;
+
   }
 
 // Function to display output
 function displayOutput(output) {
     console.log("Output:", output);
-    calculate();
-    return result;
+    return calculate();
 }
+
+var outputElement = document.getElementById("output");
+    outputElement.textContent = calculate();
 
 // Example usage
 displayOutput(add(5, 3)); // Output: 8
-displayOutput(subtract(10, 4)); // Output: 6
-displayOutput(multiply(2, 6)); // Output: 12
-displayOutput(divide(10, 2)); // Output: 5
-displayOutput(divide(10, 0)); // Output: Error: Division by zero
+// displayOutput(subtract(10, 4)); // Output: 6
+// displayOutput(multiply(2, 6)); // Output: 12
+// displayOutput(divide(10, 2)); // Output: 5
+// displayOutput(divide(10, 0)); // Output: Error: Division by zero
