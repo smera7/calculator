01c598c7f8899669ef43e24fcb979080c6d5c723
Smera <smera@berkeley.edu>
2023-12-25 00:23:00 -0800

    operator define

--- script.js
+++ script.js
@@ -6,26 +6,26 @@
 
 // Function to add two numbers
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 // Function to subtract two numbers
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 // Function to multiply two numbers
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 // Function to divide two numbers
 function divide(a, b) {
-    if (b === 0) {
-        zero_error = 'Error: Division by zero';
-        return zero_error;
-    }
-    return a / b;
+  if (b === 0) {
+      zero_error = 'Error: Division by zero';
+      return zero_error;
+  }
+  return a / b;
 }
 
 // console.log(add(2, 3));
@@ -33,9 +33,9 @@ var computationInput = document.getElementById('computation');
 
 document.querySelector('form.userinput').addEventListener('submit', function (e) {
 
-    //prevent the normal submission of the form
-    e.preventDefault();
-    console.log(computationInput.value);    
+  //prevent the normal submission of the form
+  e.preventDefault();
+  console.log(computationInput.value);    
 });
 
 // let userInput = processInput();
@@ -49,80 +49,88 @@ let tryInput; // Declare the variable at a global scopes
 
 
 document.querySelector('form.userinput').addEventListener('submit', function (e) {
-  // Prevent the normal submission of the form
-  e.preventDefault();
+// Prevent the normal submission of the form
+e.preventDefault();
 
-  // Access the input value using the declared variable
-  tryInput = document.getElementById('computation').value;
-  console.log("Inside event listener:", tryInput);
+// Access the input value using the declared variable
+tryInput = document.getElementById('computation').value;
+console.log("Inside event listener:", tryInput);
 
-  // Call a function or execute code that needs the value
-  proccesing();
-  getOperator(tryInput);
+// Call a function or execute code that needs the value
+proccesing();
+getOperator(tryInput);
 });
 
 // Function to do something with the value outside the event listener
 function proccesing() {
-  console.log("User entered:", tryInput);
-  var number1 = parseInt(tryInput);
-  console.log("Number 1:", number1);
-  operator = getOperator(tryInput);};
+console.log("User entered:", tryInput);
+var number1 = parseInt(tryInput);
+console.log("Number 1:", number1);
+operator = getOperator(tryInput);
+var number2 = getNumber2(tryInput, operator);};
 
-  function getOperator(tryInput) {
-    console.log("tryInput2:", tryInput);
-    console.log(typeof(tryInput));
-    operator = tryInput.split("")[1];
-    operator.astype()
-    var jiji = 2;
-    console.log("new:", jiji);
-    console.log("Operator:", operator);
-  };
+function getOperator(tryInput) {
+  console.log("tryInput2:", tryInput);
+    // operator = tryInput.split("")[1];
+  const matches = tryInput.match(/[+\-*/]/g);
+  console.log(matches); // Output: ["+", "*", "/", "-"]
+  console.log(typeof(matches)); // Output: object
+  operator = matches[0];
+
+  console.log("Operator:", operator);
+};
+
+function getNumber2(tryInput, operator) {
+  number2 = tryInput.split("")[1];
+  console.log("yogo", number2);
+  return parseInt(number2);
+}
 
 
-  // console.log("Operator:", operator);
-  // var number2 = getNumber2(tryInput, operator);
-  // console.log("Number 2:", number2);
+// console.log("Operator:", operator);
+// var number2 = getNumber2(tryInput, operator);
+// console.log("Number 2:", number2);
 
-  // Perform a function based on the selected option
-  // if (operator === '+') {
-  //     result = add(number1, number2);
-  // } else if (operator === '-') {
-  //     result = subtract(number1, number2);
-  // } else if (operator === '*') {
-  //     result = multiply(number1, number2);
-  // } else if (operator === '/') {
-  //     result = divide(number1, number2);
-  //     console.log(result);
-  // }
+// Perform a function based on the selected option
+// if (operator === '+') {
+//     result = add(number1, number2);
+// } else if (operator === '-') {
+//     result = subtract(number1, number2);
+// } else if (operator === '*') {
+//     result = multiply(number1, number2);
+// } else if (operator === '/') {
+//     result = divide(number1, number2);
+//     console.log(result);
+// }
 
-  console.log("tryinput1:", tryInput);
+console.log("tryinput1:", tryInput);
 
 proccesing();
 
 
 function calculatemulti() {
-  // Get the selected option value
-  // userInput = document.getElementById('computation').value;
-  // number1, operator, number2 = processInput(unprocessed);
-  var selectedOption = document.getElementsByClassName("operatormulti");
-  var a = number1;
-  var b = number2;
-  // var newop = operator;
-  console.log("Newop:", newop);
-  // Perform a function based on the selected option
-  if (newop === '+') {
-      result = add(a, b);
-  } else if (newop === '-') {
-      result = subtract(a, b);
-  } else if (newop === '*') {
-      result = multiply(a, b);
-  } else if (newop === '/') {
-      result = divide(a, b);
-      console.log(result);
+// Get the selected option value
+// userInput = document.getElementById('computation').value;
+// number1, operator, number2 = processInput(unprocessed);
+var selectedOption = document.getElementsByClassName("operatormulti");
+var a = number1;
+var b = number2;
+// var newop = operator;
+console.log("Newop:", newop);
+// Perform a function based on the selected option
+if (newop === '+') {
+    result = add(a, b);
+} else if (newop === '-') {
+    result = subtract(a, b);
+} else if (newop === '*') {
+    result = multiply(a, b);
+} else if (newop === '/') {
+    result = divide(a, b);
+    console.log(result);
 
-  }return result;}
+}return result;}
 
-  calculatemulti();
+calculatemulti();
 
 // You can also call someOtherFunction() directly if needed
 //someOtherFunction();
@@ -132,32 +140,32 @@ function calculatemulti() {
 
 // // button function on click display
 function display(button) {
-    // Get the text content of the clicked button
-    var buttonText = button.textContent;
+  // Get the text content of the clicked button
+  var buttonText = button.textContent;
 
-    // Update the value of the input element with the button text content
-    var computationInput = document.getElementById('computation');
-    computationInput.value += buttonText;
-    userInput = computationInput.value;
-    
-    if (buttonText === '=') {
-        var buttonText = button.textContent;
-        computationInput.value = '';
-        // Perform the calculation
-        result = calculatemulti();
-        console.log(result);
-        // Display the result
-        computationInput.value = result;
-    }
+  // Update the value of the input element with the button text content
+  var computationInput = document.getElementById('computation');
+  computationInput.value += buttonText;
+  userInput = computationInput.value;
+  
+  if (buttonText === '=') {
+      var buttonText = button.textContent;
+      computationInput.value = '';
+      // Perform the calculation
+      result = calculatemulti();
+      console.log(result);
+      // Display the result
+      computationInput.value = result;
   }
+}
 
-  // function getNumber2(expression, operator) {
-  //   const number2 = expression.split(operator)[1];
-  //   console.log(number2);
-  //   return parseInt(number2);}
+// function getNumber2(expression, operator) {
+//   const number2 = expression.split(operator)[1];
+//   console.log(number2);
+//   return parseInt(number2);}
 
 
 function clear(button){
-  var computationInput = document.getElementById('computation');
-  document.getElementById("output").innerText = "";
+var computationInput = document.getElementById('computation');
+document.getElementById("output").innerText = "";
 }
